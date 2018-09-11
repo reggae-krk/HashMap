25d4633d9e9fdb9d4bcb10153355d07d821f0cf7
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-11 12:31:59 +0200

    Create getValue method

--- src/main/java/HashMap.java
+++ src/main/java/HashMap.java
@@ -34,4 +34,19 @@ public class HashMap<K, V> {
             }
         }
     }
+
+    public V getValue(K key) {
+        int position;
+        Object value = null;
+        if (key == null) position = 0;
+        else position = getHash(key);
+
+        LinkedList<KeyValue> list = this.elements[position];
+
+        for (KeyValue kv : list) {
+            if(kv.key.equals(key)) value = kv.value;
+            else throw new IllegalArgumentException("There is no such key in HashMap");
+        }
+        return (V) value;
+    }
 }
